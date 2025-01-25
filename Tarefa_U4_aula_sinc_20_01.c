e94ed3dd294461b24a0e635ed8b5acdd4a95cb90
fabiorgj <fabiogomesj@gmail.com>
2025-01-25 15:09:14 -0300

    Adicionando a lógica de acionamento dos leds

--- Tarefa_U4_aula_sinc_20_01.c
+++ Tarefa_U4_aula_sinc_20_01.c
@@ -9,17 +9,135 @@
 #include "hardware/adc.h"
 #include "pico/bootrom.h"
 
+// Biblioteca gerada pelo arquivo .pio durante compilação.
+#include "ws2818b.pio.h"
 
-//mapa de teclas
-char mapa_TECLAS[4][4]={
-  '1','2','3','A',
-  '4','5','6','B',
-  '7','8','9','C',
-  '*','0','#','D'
+//////////////////////////////// ÁREA PARA DEFINIÇÃO DAS ANIMAÇÕES ///////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Definição da quantidade de frames das animações
+//  É necessário definir a quantidade de frames de cada animação
+//  As animações não podem ter mais de 15 frames
+
+#define fps 5
+
+#define repeticoes 2
+
+#define frames_animex_1 12
+#define frames_animex_2 0
+#define frames_animex_3 0
+#define frames_animex_4 0
+#define frames_animex_5 0
+#define frames_animex_6 0
+
+int frames_animex;
+
+// Definição das animações
+
+int animex_1[frames_animex_1][5][5][3] = {
+    /////////////////////////SUBSTITUIR TODOS OS SPRITES CONVERTIDOS A PARTIR DAQUI
+
+    
+    ///////////////////////////////////////////////////////////////// ATÉ AQUI
 };
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Definição do número de LEDs e pino.
+#define LED_COUNT 25
+#define LED_PIN 20
+
+// Definição de pixel GRB
+struct pixel_t
+{
+    uint8_t G, R, B; // Três valores de 8-bits compõem um pixel.
+};
+typedef struct pixel_t pixel_t;
+typedef pixel_t npLED_t; // Mudança de nome de "struct pixel_t" para "npLED_t" por clareza.
+
+// Declaração do buffer de pixels que formam a matriz.
+npLED_t leds[LED_COUNT];
+
+// Variáveis para uso da máquina PIO.
+PIO np_pio;
+uint sm;
+
+/**
+ * Inicializa a máquina PIO para controle da matriz de LEDs.
+ */
+void npInit(uint pin)
+{
+
+    // Cria programa PIO.
+    uint offset = pio_add_program(pio0, &ws2818b_program);
+    np_pio = pio0;
+
+    // Toma posse de uma máquina PIO.
+    sm = pio_claim_unused_sm(np_pio, false);
+    if (sm < 0)
+    {
+        np_pio = pio1;
+        sm = pio_claim_unused_sm(np_pio, true); // Se nenhuma máquina estiver livre, panic!
+    }
+
+    // Inicia programa na máquina PIO obtida.
+    ws2818b_program_init(np_pio, sm, offset, pin, 800000.f);
+
+    // Limpa buffer de pixels.
+    for (uint i = 0; i < LED_COUNT; ++i)
+    {
+        leds[i].R = 0;
+        leds[i].G = 0;
+        leds[i].B = 0;
+    }
+}
+
+/**
+ * Atribui uma cor RGB a um LED.
+ */
+void npSetLED(const uint index, const uint8_t r, const uint8_t g, const uint8_t b)
+{
+    leds[index].R = r;
+    leds[index].G = g;
+    leds[index].B = b;
+}
+
+/**
+ * Escreve os dados do buffer nos LEDs.
+ */
+void npWrite()
+{
+    // Escreve cada dado de 8-bits dos pixels em sequência no buffer da máquina PIO.
+    for (uint i = 0; i < LED_COUNT; ++i)
+    {
+        pio_sm_put_blocking(np_pio, sm, leds[i].G);
+        pio_sm_put_blocking(np_pio, sm, leds[i].R);
+        pio_sm_put_blocking(np_pio, sm, leds[i].B);
+    }
+    sleep_us(100); // Espera 100us, sinal de RESET do datasheet.
+}
+
+/**
+ * Limpa o buffer de pixels.
+ */
+void npClear()
+{
+    for (uint i = 0; i < LED_COUNT; ++i){
+        npSetLED(i, 0, 0, 0);
+    }
+    npWrite();
+}
+
+// mapa de teclas
+char mapa_TECLAS[4][4] = {
+    '1', '2', '3', 'A',
+    '4', '5', '6', 'B',
+    '7', '8', '9', 'C',
+    '*', '0', '#', 'D'};
+
 // Definindo os pinos para linhas e colunas
-const uint linhas[] = {8, 7, 6, 5};   // Pinos das linhas (GP5, GP6, GP7, GP8)
+const uint linhas[] = {8, 7, 6, 5};  // Pinos das linhas (GP5, GP6, GP7, GP8)
 const uint colunas[] = {4, 3, 2, 1}; // Pinos das colunas (GP1, GP2, GP3, GP4)
 
 // Protótipos das funções chamadas por cada tecla
@@ -48,7 +166,8 @@ void aciona_buzzer(void);
 #define BUZZER_FREQUENCY 2000
 
 // Função para inicializar o PWM no pino do buzzer
-void init_buzzer(uint pin) {
+void init_buzzer(uint pin)
+{
     // Configurar o pino como saída de PWM
     gpio_set_function(pin, GPIO_FUNC_PWM);
 
@@ -60,163 +179,246 @@ void init_buzzer(uint pin) {
     pwm_config_set_clkdiv(&config, clock_get_hz(clk_sys) / (BUZZER_FREQUENCY * 4096)); // Divisor de clock
     pwm_init(slice_num, &config, true);
 
-
     // Iniciar o PWM no nível baixo
     pwm_set_gpio_level(pin, 0);
 }
 
-// Inicializando os GPIOs do teclado 
-void inicializa_teclado() {
+// Inicializando os GPIOs do teclado
+void inicializa_teclado()
+{
     // Configurando as linhas como saída
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++)
+    {
         gpio_init(linhas[i]);
         gpio_set_dir(linhas[i], GPIO_OUT);
         gpio_put(linhas[i], 1); // Mantendo em nível alto
     }
 
     // Configurando as colunas como entrada com pull-up
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++)
+    {
         gpio_init(colunas[i]);
         gpio_set_dir(colunas[i], GPIO_IN);
         gpio_pull_up(colunas[i]);
     }
 }
 
-//Lendo a tecla pressionada no teclado 
-char ler_tecla() {
-    for (int i = 0; i < 4; i++) {
-        //Colocando a linha atual em nível baixo
+// Lendo a tecla pressionada no teclado
+char ler_tecla()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        // Colocando a linha atual em nível baixo
         gpio_put(linhas[i], 0);
 
-        //Verificando se alguma coluna está em nível baixo
-        for (int j = 0; j < 4; j++) {
-            if (gpio_get(colunas[j]) == 0) { //Coluna em nível baixo
-                sleep_ms(20); //intervalo simples
-                if (gpio_get(colunas[j]) == 0) {
-                    //Restaurandi a linha para nível alto
+        // Verificando se alguma coluna está em nível baixo
+        for (int j = 0; j < 4; j++)
+        {
+            if (gpio_get(colunas[j]) == 0)
+            {                 // Coluna em nível baixo
+                sleep_ms(20); // intervalo simples
+                if (gpio_get(colunas[j]) == 0)
+                {
+                    // Restaurandi a linha para nível alto
                     gpio_put(linhas[i], 1);
                     return mapa_TECLAS[i][j];
                 }
             }
         }
 
-        //Restaurando a linha para nível alto
+        // Restaurando a linha para nível alto
         gpio_put(linhas[i], 1);
     }
-    return '\0'; //Sem teclas pressionadas
+    return '\0'; // Sem teclas pressionadas
 }
 
-//main
-int main(){
-  //inicializações
-  stdio_init_all();
-  init_buzzer(BUZZER_PIN);
-  inicializa_teclado();
-  while(true){
-    //lendo a tecla
-    char tecla = ler_tecla();
-    char last_tecla;
-    char escolha_switch;
-
-    if (tecla != last_tecla) {
-        last_tecla = tecla;
-        escolha_switch = tecla;
-    }else{
-      sleep_ms(50);
-      escolha_switch = 'N';
+// Função para converter a posição do matriz para uma posição do vetor.
+int getIndex(int x, int y)
+{
+    // Se a linha for par (0, 2, 4), percorremos da esquerda para a direita.
+    // Se a linha for ímpar (1, 3), percorremos da direita para a esquerda.
+    if (y % 2 == 0)
+    {
+        return 24 - (y * 5 + x); // Linha par (esquerda para direita).
     }
-    //se for 8,chama a função que liga todos os LEDs
-    switch (escolha_switch) {
-        case '0': func_0(); break;
-        case '1': func_1(); break;
-        case '2': func_2(); break;
-        case '3': func_3(); break;
-        case '4': func_4(); break;
-        case '5': func_5(); break;
-        case '6': func_6(); break;
-        case '7': func_7(); break;
-        case '8': func_8(); break;
-        case '9': func_9(); break;
-        case 'A': func_A(); break;
-        case 'B': func_B(); break;
-        case 'C': func_C(); break;
-        case 'D': func_D(); break;
-        case '*': func_reboot(); break;
-        case '#': func_hash; break;
-        default: break; // Nenhuma tecla válida
+    else
+    {
+        return 24 - (y * 5 + (4 - x)); // Linha ímpar (direita para esquerda).
     }
-  }
-return 0;
 }
 
-void func_0(void){
-    aciona_buzzer();
+// Função para fazer a animação na matriz de leds
+void desenhar_sprite(int animacao[frames_animex][5][5][3], int sprite)
+{
+    // Desenhando Sprite contido na animex[?][]
+    for (int linha = 0; linha < 5; linha++)
+    {
+        for (int coluna = 0; coluna < 5; coluna++)
+        {
+            int posicao = getIndex(linha, coluna);
+            npSetLED(posicao, animacao[sprite][coluna][linha][0], animacao[sprite][coluna][linha][1], animacao[sprite][coluna][linha][2]);
+        }
+    }
 }
 
-void func_1(void){
+// main
+int main()
+{
+    // inicializações
+    stdio_init_all();
+    init_buzzer(BUZZER_PIN);
+    inicializa_teclado();
+    npInit(LED_PIN);
+    npClear();
+    npWrite();
+
+    while (true)
+    {
+        // lendo a tecla
+        char tecla = ler_tecla();
+        char last_tecla;
+        char escolha_switch;
+
+        if (tecla != last_tecla)
+        {
+            last_tecla = tecla;
+            escolha_switch = tecla;
+        }
+        else
+        {
+            sleep_ms(50);
+            escolha_switch = 'N';
+        }
+        // se for 8,chama a função que liga todos os LEDs
+        switch (escolha_switch)
+        {
+        case '0':
+            func_0();
+            break;
+        case '1':
+            func_1();
+            break;
+        case '2':
+            func_2();
+            break;
+        case '3':
+            func_3();
+            break;
+        case '4':
+            func_4();
+            break;
+        case '5':
+            func_5();
+            break;
+        case '6':
+            func_6();
+            break;
+        case '7':
+            func_7();
+            break;
+        case '8':
+            func_8();
+            break;
+        case '9':
+            func_9();
+            break;
+        case 'A':
+            func_A();
+            break;
+        case 'B':
+            func_B();
+            break;
+        case 'C':
+            func_C();
+            break;
+        case 'D':
+            func_D();
+            break;
+        case '*':
+            func_reboot();
+            break;
+        case '#':
+            func_hash;
+            break;
+        default:
+            break; // Nenhuma tecla válida
+        }
+    }
+    return 0;
+}
+
+void func_0(void)
+{
+}
+
+void func_1(void)
+{
     
 }
 
-void func_2(void){
-    
-}
-
-void func_3(void){
-    
-}
-
-void func_4(void){
-    
-}
-
-void func_5(void){
-    
-}
-
-void func_6(void){
-    
-}
-
-void func_7(void){
-    
-}
-
-void func_8(void){
-    
-}
-
-void func_9(void){
-    
-}
-
-void func_A(void){
-    
-}
-
-void func_B(void){
-    
-}
-
-void func_C(void){
+void func_2(void)
+{
 
 }
 
-void func_D(void){
-    
+void func_3(void)
+{
 }
 
-void func_reboot(void){
+void func_4(void)
+{
+}
+
+void func_5(void)
+{
+}
+
+void func_6(void)
+{
+}
+
+void func_7(void)
+{
+}
+
+void func_8(void)
+{
+}
+
+void func_9(void)
+{
+}
+
+void func_A(void)
+{
+}
+
+void func_B(void)
+{
+}
+
+void func_C(void)
+{
+}
+
+void func_D(void)
+{
+}
+
+void func_reboot(void)
+{
     printf("Dando reboot...\n");
     sleep_ms(1000);
-    reset_usb_boot(0,0);
+    reset_usb_boot(0, 0);
 }
 
-void func_hash(void){
-    
+void func_hash(void)
+{
 }
 
-//Aciona o buzzer por meio segundo
-void aciona_buzzer(void) {
+// Aciona o buzzer
+void aciona_buzzer(void)
+{
     // Obter o slice do PWM associado ao pino
     uint slice_num = pwm_gpio_to_slice_num(BUZZER_PIN);
 
@@ -227,7 +429,7 @@ void aciona_buzzer(void) {
     printf("Buzz\n");
 
     // Temporização para duração do som
-    sleep_ms(500);
+    sleep_ms(1000 / fps);
 
     // Desativar o PWM (duty cycle 0)
     pwm_set_gpio_level(BUZZER_PIN, 0);
